7458313e3c1598d695eecde9375b8e0bfddb6e41
zekegoodman <zekegoodman@ZEKEGOODMANB9EC>
2024-02-02 15:06:34 -0700

    too much to explain

--- Program.cs
+++ Program.cs
@@ -17,22 +17,14 @@ grid[2] = new string[] { "-", "-", "-" };
 // initialize other varriables
 string[] possibleGuesses = new string[9];
 string p1guess = "";
+string p2guess = "";
 string winner;
-bool turnTaken = true;
+bool guessValidation = false;
 Board board = new Board();
-
-//string A1 = grid[0][0];
-//string B1 = grid[0][1];
-//string C1 = grid[0][2];
-//string A2 = grid[1][0];
-//string B2 = grid[1][1];
-//string C2 = grid[1][2];
-//string A3 = grid[2][0];
-//string B3 = grid[2][1];
-//string C3 = grid[2][2];
+string[] acceptableInputs = { "A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3" };
 
 // Welcome the user
-Console.WriteLine("Welcome to the game!");
+Console.WriteLine("Welcome to the game! Player 1 is Xs and player 2 is Os");
 
 // Give user guideance
 Console.WriteLine("Please enter your selection by typing the column letter followed by the row number (I.E: A1) ");
@@ -45,138 +37,154 @@ Console.WriteLine("3: -, -, -");
 for (int iCount = 0; iCount < 9; iCount++)
 {
     // Ask each player in turn for their choice 
-    Console.WriteLine("Player 1 please take your turn");
-    p1guess = Console.ReadLine().ToUpper();
-    turnTaken = true;
-    while(turnTaken == true)
+    
+
+    if (iCount == 0 || iCount == 2 || iCount == 4 || iCount == 6 || iCount == 8)
     {
+        Console.WriteLine("Player 1 please take your turn");
+        p1guess = Console.ReadLine().ToUpper();
+        guessValidation = false;
+
+        while (guessValidation == false)
+        {
+            if (acceptableInputs.Contains(p1guess))
+            {
+                guessValidation = true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid input by typing the column letter followed by the row number (I.E: A1)");
+                p1guess = Console.ReadLine().ToUpper();
+            }
+
+
+        }
+
         if (p1guess == "A1" && grid[0][0] == "-")
         {
             grid[0][0] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "B1" && grid[1][0] == "-")
         {
             grid[1][0] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "C1" && grid[2][0] == "-")
         {
             grid[2][0] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "A2" && grid[0][1] == "-")
         {
             grid[0][1] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "B2" && grid[1][1] == "-")
         {
             grid[1][1] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "C2" && grid[2][1] == "-")
         {
             grid[2][1] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "A3" && grid[0][2] == "-")
         {
             grid[0][2] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "B3" && grid[1][2] == "-")
         {
             grid[1][2] = "x";
-            turnTaken = false;
         }
         else if (p1guess == "C3" && grid[2][2] == "-")
         {
             grid[2][2] = "x";
-            turnTaken = false;
         }
         else
-            Console.WriteLine("That spot is taken");
-    }
-    
-
-    board.printBoard(grid);
-
-    iCount++;
-
-    //grid[0][1] = "x";
-    //Console.Write(B1);
-    //Console.WriteLine(grid[0][1]);
-    winner = board.checkWinner(grid);
-
-    if (winner == "x")
-    {
-        Console.WriteLine("Player 1 wins!!");
-        iCount = 9;
-    }
-    if (winner == "o")
-    {
-        Console.WriteLine("Player 2 wins!!");
-        iCount = 9;
-    }
-
-    
-
-    if(iCount < 9)
-    {
-        Console.WriteLine("Player 2 please take your turn");
-        p1guess = Console.ReadLine().ToUpper();
-        turnTaken = true;
-
-        while (turnTaken == true)
         {
-            if (p1guess == "A1" && grid[0][0] == "-")
+            Console.WriteLine("That spot is taken");
+            iCount--;
+        }
+
+
+        board.printBoard(grid);
+
+        //grid[0][1] = "x";
+        //Console.Write(B1);
+        //Console.WriteLine(grid[0][1]);
+        winner = board.checkWinner(grid);
+
+        if (winner == "x")
+        {
+            Console.WriteLine("Player 1 wins!!");
+            iCount = 9;
+        }
+        if (winner == "o")
+        {
+            Console.WriteLine("Player 2 wins!!");
+            iCount = 9;
+        }
+    }
+    else
+    {
+        if (iCount < 9)
+        {
+            Console.WriteLine("Player 2 please take your turn");
+            p2guess = Console.ReadLine().ToUpper();
+
+            guessValidation = false;
+
+            while (guessValidation == false)
+            {
+                if (acceptableInputs.Contains(p2guess))
+                { guessValidation = true; }
+                else
+                {
+                    Console.WriteLine("Please enter a valid input by typing the column letter followed by the row number (I.E: A1)");
+                    p2guess = Console.ReadLine().ToUpper();
+                }
+
+
+            }
+
+
+            if (p2guess == "A1" && grid[0][0] == "-")
             {
                 grid[0][0] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "B1" && grid[1][0] == "-")
+            else if (p2guess == "B1" && grid[1][0] == "-")
             {
                 grid[1][0] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "C1" && grid[2][0] == "-")
+            else if (p2guess == "C1" && grid[2][0] == "-")
             {
                 grid[2][0] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "A2" && grid[0][1] == "-")
+            else if (p2guess == "A2" && grid[0][1] == "-")
             {
                 grid[0][1] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "B2" && grid[1][1] == "-")
+            else if (p2guess == "B2" && grid[1][1] == "-")
             {
                 grid[1][1] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "C2" && grid[2][1] == "-")
+            else if (p2guess == "C2" && grid[2][1] == "-")
             {
                 grid[2][1] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "A3" && grid[0][2] == "-")
+            else if (p2guess == "A3" && grid[0][2] == "-")
             {
                 grid[0][2] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "B3" && grid[1][2] == "-")
+            else if (p2guess == "B3" && grid[1][2] == "-")
             {
                 grid[1][2] = "o";
-                turnTaken = false;
             }
-            else if (p1guess == "C3" && grid[2][2] == "-")
+            else if (p2guess == "C3" && grid[2][2] == "-")
             {
                 grid[2][2] = "o";
-                turnTaken = false;
             }
             else
+            {
                 Console.WriteLine("That spot is taken");
+                iCount--;
+            }
 
             board.printBoard(grid);
 
@@ -193,7 +201,6 @@ for (int iCount = 0; iCount < 9; iCount++)
                 iCount = 9;
             }
         }
-
     }
 
 }
