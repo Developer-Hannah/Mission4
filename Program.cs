53b9269a56525a70a6cab06b4216a9841b2a49aa
Developer-Hannah <157655455+Developer-Hannah@users.noreply.github.com>
2024-02-02 12:31:20 -0700

    i

--- Program.cs
+++ Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Board class
+//printBoard([[], [], []]) method
+
+// Create an array to hold each of three lines of the grid
+using System.Security.Cryptography;
+using Bo
+
+// initialize array
+string[][] grid = new string[3][];
+grid[0] = new string[] { "-", "-", "-" };
+grid[1] = new string[] { "-", "-", "-" };
+grid[2] = new string[] { "-", "-", "-" };
+
+// initialize other varriables
+string[] possibleGuesses = new string[9];
+string p1guess = "";
+//string A1 = grid[0][0];
+//string B1 = grid[0][1];
+//string C1 = grid[0][2];
+//string A2 = grid[1][0];
+//string B2 = grid[1][1];
+//string C2 = grid[1][2];
+//string A3 = grid[2][0];
+//string B3 = grid[2][1];
+//string C3 = grid[2][2];
+
+// Welcome the user
+Console.WriteLine("Welcome to the game!");
+
+// Give user guideance
+Console.WriteLine("Please enter your selection by typing the column letter followed by the row number (I.E: A1) ");
+
+Console.WriteLine("   A, B, C");
+Console.WriteLine("1: -, -, -");
+Console.WriteLine("2: -, -, -");
+Console.WriteLine("3: -, -, -");
+
+// Ask each player in turn for their choice 
+Console.WriteLine("Player 1 please take your turn");
+p1guess = Console.ReadLine();
+
+if (p1guess == "A1" && grid[0][0] == "-")
+    grid[0][0] = "x";
+else if (p1guess == "B1" && grid[0][1] == "-")
+    grid[0][1] = "x";
+else if (p1guess == "C1" && grid[0][2] == "-")
+    grid[0][2] = "x";
+else if (p1guess == "A2" && grid[1][0] == "-")
+    grid[1][0] = "x";
+else if (p1guess == "B2" && grid[1][1] == "-")
+    grid[1][1] = "x";
+else if (p1guess == "C2" && grid[1][2] == "-")
+    grid[1][2] = "x";
+else if (p1guess == "A3" && grid[2][0] == "-")
+    grid[2][0] = "x";
+else if (p1guess == "B3" && grid[2][1] == "-")
+    grid[2][1] = "x";
+else if (p1guess == "C3" && grid[2][2] == "-")
+    grid[2][2] = "x";
+else
+    Console.WriteLine("That spot is taken");
+
+//grid[0][1] = "x";
+//Console.Write(B1);
+//Console.WriteLine(grid[0][1]);
+
+Console.WriteLine("Player 2 please take your turn");
+p1guess = Console.ReadLine();
+
+if (p1guess == "A1" && grid[0][0] == "-")
+    grid[0][0] = "o";
+else if (p1guess == "B1" && grid[0][1] == "-")
+    grid[0][1] = "o";
+else if (p1guess == "C1" && grid[0][2] == "-")
+    grid[0][2] = "o";
+else if (p1guess == "A2" && grid[1][0] == "-")
+    grid[1][0] = "o";
+else if (p1guess == "B2" && grid[1][1] == "-")
+    grid[1][1] = "o";
+else if (p1guess == "C2" && grid[1][2] == "-")
+    grid[1][2] = "o";
+else if (p1guess == "A3" && grid[2][0] == "-")
+    grid[2][0] = "o";
+else if (p1guess == "B3" && grid[2][1] == "-")
+    grid[2][1] = "o";
+else if (p1guess == "C3" && grid[2][2] == "-")
+    grid[2][2] = "o";
+else
+Console.WriteLine("That spot is taken");
+
+// Print the board by calling the method in the supporting class
+//printBoard()
+
+//Check for a winnner by calling the method in the supporting class, and notify the players 
+//when a win has occurred and which player won the game
+
+
